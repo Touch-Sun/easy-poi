76858ef2ec2c5ef67d6e0947306b5cb46639a4ed
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-07-11 18:29:59 +0800

    feat: 添加EasyPoi案例启动类

---
+++ src/main/java/com/touchsun/easypoi/EasyPoi.java
@@ -0,0 +1,12 @@
+package com.touchsun.easypoi;
+
+/**
+ * 示例启动类
+ *
+ * @author Lee
+ */
+public class EasyPoi {
+    public static void main(String[] args) {
+        System.out.println("Hello EasyPOI!");
+    }
+}
