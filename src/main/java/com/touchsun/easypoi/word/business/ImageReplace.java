e38e206ea7158919e2a058d07039604fdb4f581a
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-07-14 10:56:14 +0800

    feat: 增加业务工具[Word][替换文档中某一位置的图片]

--- src/main/java/com/touchsun/easypoi/word/business/ImageReplace.java
+++ src/main/java/com/touchsun/easypoi/word/business/ImageReplace.java
@@ -1,5 +1,25 @@
 package com.touchsun.easypoi.word.business;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.PackagePart;
+import org.apache.poi.xwpf.usermodel.Document;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlToken;
+import org.openxmlformats.schemas.drawingml.x2006.main.CTNonVisualDrawingProps;
+import org.openxmlformats.schemas.drawingml.x2006.main.CTPositiveSize2D;
+import org.openxmlformats.schemas.drawingml.x2006.wordprocessingDrawing.CTInline;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 业务场景<br/>
  * 需求：替换Word中的图片为新的图片<br/>
@@ -12,4 +32,138 @@ package com.touchsun.easypoi.word.business;
  */
 public class ImageReplace extends Business {
 
+
+    /**
+     * 替换指定ID处的图片
+     *
+     * @param picId 图片在WordXML中的ID
+     * @param pictureStream 新的图片输入流
+     * @param docx 文档对象
+     */
+    public static void replacePicture(String picId, InputStream pictureStream, XWPFDocument docx)
+            throws InvalidFormatException {
+        List<XWPFParagraph> paragraphs = docx.getParagraphs();
+        for (XWPFParagraph paragraph : paragraphs) {
+            List<XWPFRun> runs = paragraph.getRuns();
+            if(CollectionUtils.isNotEmpty(runs)) {
+                XWPFRun run = runs.get(0);
+                Node node = run.getCTR().getDomNode();
+                // drawing 一个绘画的图片
+                Node drawingNode = getChildNode(node, "w:drawing");
+                if (drawingNode == null) {
+                    continue;
+                }
+                // 绘画图片的宽和高
+                Node extentNode = getChildNode(drawingNode, "wp:extent");
+                NamedNodeMap extentAttrs = Objects.requireNonNull(extentNode).getAttributes();
+                // 绘画图片具体引用
+                Node blipNode = getChildNode(drawingNode, "a:blip");
+                NamedNodeMap blipAttrs = Objects.requireNonNull(blipNode).getAttributes();
+                String rid = blipAttrs.getNamedItem("r:embed").getNodeValue();
+                if(rid.equals(picId)) {
+                    System.out.println("找到图片ID: " + picId);
+                    // 获取图片信息
+                    PackagePart part = docx.getPartById(rid);
+                    // 删除此节点
+                    node.removeChild(drawingNode);
+                    // 此节点处添加图片
+                    String newPicId = docx.addPictureData(pictureStream, Document.PICTURE_TYPE_PNG);
+                    // 添加图片
+                    addPictureToRun(run,newPicId,Document.PICTURE_TYPE_PNG,
+                            Integer.parseInt(extentAttrs.getNamedItem("cx").getNodeValue()),
+                            Integer.parseInt(extentAttrs.getNamedItem("cy").getNodeValue()));
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 获取一个节点的子节点
+     *
+     * @param node 节点
+     * @param nodeName 节点名称
+     * @return 子节点
+     */
+    private static Node getChildNode(Node node, String nodeName) {
+        if (!node.hasChildNodes()) {
+            return null;
+        }
+        NodeList childNodes = node.getChildNodes();
+
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node childNode = childNodes.item(i);
+            if (nodeName.equals(childNode.getNodeName())) {
+                return childNode;
+            }
+            childNode = getChildNode(childNode, nodeName);
+            if (childNode != null) {
+                return childNode;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 添加图片到run
+     *
+     * @param run run对象
+     * @param blipId blipId
+     * @param id 节点ID
+     * @param width 图片宽度
+     * @param height 图片高度
+     */
+    private static void addPictureToRun(XWPFRun run,String blipId,int id,long width, long height){
+
+        CTInline inline =run.getCTR().addNewDrawing().addNewInline();
+
+        String picXml = "" +
+                "<a:graphic xmlns:a=\"http://schemas.openxmlformats.org/drawingml/2006/main\">" +
+                "   <a:graphicData uri=\"http://schemas.openxmlformats.org/drawingml/2006/picture\">" +
+                "      <pic:pic xmlns:pic=\"http://schemas.openxmlformats.org/drawingml/2006/picture\">" +
+                "         <pic:nvPicPr>" +
+                "            <pic:cNvPr id=\"" + id + "\" name=\"Generated\"/>" +
+                "            <pic:cNvPicPr/>" +
+                "         </pic:nvPicPr>" +
+                "         <pic:blipFill>" +
+                "            <a:blip r:embed=\"" + blipId + "\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\"/>" +
+                "            <a:stretch>" +
+                "               <a:fillRect/>" +
+                "            </a:stretch>" +
+                "         </pic:blipFill>" +
+                "         <pic:spPr>" +
+                "            <a:xfrm>" +
+                "               <a:off x=\"0\" y=\"0\"/>" +
+                "               <a:ext cx=\"" + width + "\" cy=\"" + height + "\"/>" +
+                "            </a:xfrm>" +
+                "            <a:prstGeom prst=\"rect\">" +
+                "               <a:avLst/>" +
+                "            </a:prstGeom>" +
+                "         </pic:spPr>" +
+                "      </pic:pic>" +
+                "   </a:graphicData>" +
+                "</a:graphic>";
+
+        XmlToken xmlToken = null;
+        try {
+            xmlToken = XmlToken.Factory.parse(picXml);
+        } catch(XmlException xe) {
+            xe.printStackTrace();
+        }
+        inline.set(xmlToken);
+
+        inline.setDistT(0);
+        inline.setDistB(0);
+        inline.setDistL(0);
+        inline.setDistR(0);
+
+        CTPositiveSize2D extent = inline.addNewExtent();
+        extent.setCx(width);
+        extent.setCy(height);
+
+        CTNonVisualDrawingProps docPr = inline.addNewDocPr();
+        docPr.setId(id);
+        docPr.setName("Picture " + id);
+        docPr.setDescr("Generated");
+    }
 }
--- src/main/java/com/touchsun/easypoi/word/example/ImageReplaceExample.java
+++ src/main/java/com/touchsun/easypoi/word/example/ImageReplaceExample.java
@@ -1,14 +1,46 @@
 package com.touchsun.easypoi.word.example;
 
+
+import com.touchsun.easypoi.word.business.ImageReplace;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * Word图片替换案例
  *
  * @author Lee
  */
 public class ImageReplaceExample {
-    public static void main(String[] args) {
-        // 文件地址
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+        // 原Word文件地址
         String filePath = "D:\\backup\\2022\\0713\\POITest.docx";
+        // 替换后新的Word文件地址
+        String newFilePath = "D:\\backup\\2022\\0713\\new22POITest.docx";
+        // 要替换的备用图片
+        String imgPath = "D:\\backup\\2022\\0712\\bj.jpeg";
+        // 1. 读取为XWPFDocument
+        XWPFDocument docx = readDoc(filePath);
+        // 2. 根据分析出来的XML中图片的ID替换为新的流
+        ImageReplace.replacePicture("rId6", new FileInputStream(imgPath), docx);
+        // 3. 保存文件
+        OutputStream outputStream = new FileOutputStream(newFilePath);
+        docx.write(outputStream);
+    }
 
+    /**
+     * 拿到文档结构
+     *
+     * @param path 原Word地址
+     * @return XWPFDocument对象
+     * @throws IOException IO异常
+     */
+    public static XWPFDocument readDoc(String path) throws IOException, InvalidFormatException {
+        return new XWPFDocument(OPCPackage.open(path));
     }
 }
